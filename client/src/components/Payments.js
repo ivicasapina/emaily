b19d77dc0483231c1669d8a2b28800bf2e47a77d
Ivica Šapina <ivica.moke@gmail.com>
2017-10-30 14:27:20 +0100

    Fixin stripe key 5

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import StripeCheckout from 'react-stripe-checkout';
 import * as actions from '../actions';
+const key = 'pk_test_sN3mn431fRTHwpKrOVFhab7k';
 
 class Payments extends Component {
   render() {
@@ -11,7 +12,7 @@ class Payments extends Component {
         description="$5 for 5 survey credits"
         amount={500}
         token={token => this.props.handleToken(token)}
-        stripeKey={pk_test_sN3mn431fRTHwpKrOVFhab7k}
+        stripeKey={key}
       >
         <button className="btn">Add Credits</button>
       </StripeCheckout>
